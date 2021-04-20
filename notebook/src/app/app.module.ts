34a168b2f14f5e3ca8af81142ad5d7f92a71d175
muralitharan <muralitharan@aagnia.com>
2021-04-20 12:18:52 +0530

    login basic style completed

--- notebook/src/app/app.module.ts
+++ notebook/src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
