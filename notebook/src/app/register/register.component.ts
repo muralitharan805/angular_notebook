e0b7f8e481027da6e3b8f1fb5e45b07198cfa30a
muralitharan <muralitharan@aagnia.com>
2021-04-21 10:51:01 +0530

    nv

--- notebook/src/app/register/register.component.ts
+++ notebook/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,29 +9,37 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   hide = true;
-  register = new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phone:new FormControl('',[Validators.required,Validators.pattern("[5-9]{1}[0-9]{9}")]),
-    password: new FormControl('',[Validators.required])
+  register:any;
+
   
-  })
+  constructor(private _fb:FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.register= this._fb.group({
+      name : ['',[Validators.required]],
+      email : ['',[Validators.required]],
+      phone : ['',[Validators.required]],
+      password : ['',[Validators.required]],
+      conform_password : ['',[Validators.required]],
+
+    })
+    // this.register = new FormGroup({
+    //   name: new FormControl('',[Validators.required]),
+    //   email: new FormControl('', [Validators.required, Validators.email]),
+    //   phone:new FormControl('',[Validators.required,Validators.pattern("[5-9]{1}[0-9]{9}")]),
+    //   password: new FormControl('',[Validators.required])
+    
+    // })
+  }
 
   onSubmit(){
     console.log(this.register.value);
     
   }
-  // getErrorMessage() {
-  //   if (this.register.hashasError('required')) {
-  //     return 'You must enter a value';
-  //   }
 
-  //   return this.email.hasError('email') ? 'Not a valid email' : '';
-  // }
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get name(){
+    return this.register.get('name');
   }
 
 }
