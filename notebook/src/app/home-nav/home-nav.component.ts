078f6fb1e1df636803b2d15defcab1c0bfa1622f
muralitharan <muralitharan@aagnia.com>
2021-04-22 15:26:41 +0530

    test

--- notebook/src/app/home-nav/home-nav.component.ts
+++ notebook/src/app/home-nav/home-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-home-nav',
@@ -10,16 +11,24 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class HomeNavComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-    
+  isCheck:any;
+
+  links = ['First', 'Second', 'Third'];
+  activeLink = this.links[0];
+ 
 
   constructor(private breakpointObserver: BreakpointObserver) {
     
-    
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.isCheck=true
+      }else{
+        this.isCheck=false
+
+      }
+    }); 
   }
 
 }
