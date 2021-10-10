249a3ffca8cd2b03c52090810bc0b53749cd607f
muralitharan0506 <muralitharan@aagnia.com>
2021-10-10 13:49:40 +0530

    sf

--- notebook/src/app/app.module.ts
+++ notebook/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemomaterialModule } from './demomaterial/demomaterial.module';
+import { Child1Component } from './child1/child1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Child1Component
   ],
   imports: [
     BrowserModule,
