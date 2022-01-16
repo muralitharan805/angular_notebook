8d8d3eb6691e6d68e73bd64f2abeec1bdd4c1352
muralitharan <muralitharan805@gmail.com>
2022-01-16 16:57:23 +0530

    angular forms

--- notebook/src/app/app.component.ts
+++ notebook/src/app/app.component.ts
@@ -1,20 +1,97 @@
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  name = new FormControl('');
+
   title = 'notebook';
-  isAlternateMode=false
-  constructor(public overlayContainer: OverlayContainer){
-    
+  isAlternateMode = false;
+  constructor(
+    public overlayContainer: OverlayContainer,
+    private fb: FormBuilder
+  ) {}
+
+  check() {
+    this.overlayContainer
+      .getContainerElement()
+      .classList.add('unicorn-dark-theme');
+    this.isAlternateMode = !this.isAlternateMode;
   }
-  
-  check(){
-    this.overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
-    this.isAlternateMode=!this.isAlternateMode
+
+  updateName() {
+    this.name.setValue('Nancy');
+  }
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
+  profileForm_nestedform = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+  });
+
+  updateProfile() {
+    this.profileForm_nestedform.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+
+  profileForm_formbuilder = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+  });
+
+  profileForm_formArray = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.fb.array([this.fb.control('')]),
+  });
+
+  get aliases() {
+    return this.profileForm_formArray.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 }
--- notebook/src/app/app.module.ts
+++ notebook/src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemomaterialModule } from './demomaterial/demomaterial.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
-    DemomaterialModule
+    DemomaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
