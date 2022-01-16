1a207e7df05348724eaeeee04b5ea9ebe8454f33
muralitharan <muralitharan805@gmail.com>
2022-01-16 19:28:55 +0530

    theming example

--- notebook/src/app/app.component.ts
+++ notebook/src/app/app.component.ts
@@ -4,17 +4,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'notebook';
-  isAlternateMode=false
-  constructor(public overlayContainer: OverlayContainer){
-    
-  }
-  
-  check(){
-    this.overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
-    this.isAlternateMode=!this.isAlternateMode
+  isAlternateMode = false;
+  constructor(public overlayContainer: OverlayContainer) {}
+
+  check() {
+    this.overlayContainer
+      .getContainerElement()
+      .classList.add('unicorn-dark-theme');
+
+    // this.isAlternateMode=!this.isAlternateMode
   }
 }
