c0dce57f050f0acd6d49603b3dfcf19c378f0702
muralitharan <muralitharan805@gmail.com>
2022-02-24 07:03:50 +0530

    header side nav

--- notebook/src/app/shared/shared.module.ts
+++ notebook/src/app/shared/shared.module.ts
@@ -7,10 +7,22 @@ import { HeaderComponent } from './header/header.component';
 import { AuthGuard } from './auth.guard';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterModule } from '@angular/router';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [LoderComponent, ShowErrComponent, HeaderComponent],
+  declarations: [
+    LoderComponent,
+    ShowErrComponent,
+    HeaderComponent,
+    SideNavComponent,
+  ],
   imports: [CommonModule, DemomaterialModule, RouterModule],
-  exports: [LoderComponent, ShowErrComponent, HeaderComponent, RouterModule],
+  exports: [
+    LoderComponent,
+    ShowErrComponent,
+    HeaderComponent,
+    RouterModule,
+    SideNavComponent,
+  ],
 })
 export class SharedModule {}
